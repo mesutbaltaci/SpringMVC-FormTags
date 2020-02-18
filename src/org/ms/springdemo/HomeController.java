0fbfc2ec08fe7cfdd86457894195292df112837d
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-02-18 10:57:43 -0500

    Form input tags

---
+++ src/org/ms/springdemo/HomeController.java
@@ -0,0 +1,16 @@
+package org.ms.springdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showForm() {
+		
+		return "index";
+	}
+	
+
+}
---
+++ src/org/ms/springdemo/StudentController.java
@@ -0,0 +1,28 @@
+package org.ms.springdemo;
+
+import org.ms.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	@RequestMapping("/showForm")
+	public String showForm (Model model) {
+		Student s = new Student();
+		model.addAttribute("student",s);
+		return "student-form";
+	}
+	
+	
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student student) {
+		
+		System.out.println("Student:" + student.getFirstName() + "  " +student.getLastName());
+		
+		return "student-conf";
+	}
+}
